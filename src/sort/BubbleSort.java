2f7d54f2f9f5a6a328c1938b66fde1e79eb9acd6
choijungwoo493 <jwoo1016@gmail.com>
2022-06-24 21:50:06 +0900

    버블정렬

--- src/sort/BubbleSort.java
+++ src/sort/BubbleSort.java
@@ -3,9 +3,14 @@ package sort;
 
 public class BubbleSort {
     /**
-     1. main method 가 array 라는 변수를 읽으려면 array 변수를 static 변수로 바꿔 클래스내의 모든 범위에서 사용할 수 있게하거나
-     2. main method 안에 new BubbleSort() 로 객체를 생성해 객체를 통해 array 변수를 참조하는 방법이 있습니다.
-     3. 또, main method 안에 array 변수를 넣어 변수의 scope 를 줄여 참조하는 방법이 있습니다.
+     * 리드미컬하게 구현하기 위해 bubbleSort 에 option + enter 를 했을 겁니다. 하지만 잘 안됩니다.
+     * 현재 상태론 bubbleSort 메서드가 매개변수인 array 를 참조할 수 없기 때문입니다.
+     * 따라서 array 변수를 main 함수의 scope 안에 넣어주어야합니다.
+     *
+     * 그 방법은 세가지가 있습니다.
+     * 4-1 array 변수를 static 변수로 선언해 해당 클래스 내에서 참조할 수 있게 한다.
+     * 4-2 array 변수를 쓸 수 있도록 main 함수안에 BubbleSort 객체를 생성해 참조한다.
+     * 4-3 main 함수 안에 array 변수를 집어 넣는다.
      */
     int[] array = new int[]{1,2,5,6,7,3,4,2};
 
