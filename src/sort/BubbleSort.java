3d8eb021224412325b67e3acbae40bdaadc3603e
choijungwoo493 <jwoo1016@gmail.com>
2022-06-24 21:11:54 +0900

    버블정렬

--- src/sort/BubbleSort.java
+++ src/sort/BubbleSort.java
@@ -4,7 +4,7 @@ package sort;
 public class BubbleSort {
     int[] array = new int[]{1,2,5,6,7,3,4,2};
 
-    //todo array를 버블정렬합니다.
+    //todo array를 버블정렬합니다. 그런데 array가 빨간줄이 그어지네요?
     public static void main(String[] args) {
         bubbleSort(array);
     }
