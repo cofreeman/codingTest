eb9f667cdb66c8e93e441cfc568f6d58426d8049
choijungwoo493 <jwoo1016@gmail.com>
2022-06-24 20:59:33 +0900

    버블정렬

---
+++ src/sort/BubbleSort.java
@@ -0,0 +1,11 @@
+//question 문제가 잘못된 점이 있을까요?
+package sort;
+
+public class BubbleSort {
+    int[] array = new int[]{1,2,5,6,7,3,4,2};
+
+    //todo: 버블 정렬을 이용해 주어진 배열을 오름차순으로 출력하세요
+    public static void bubbleSort(int array) {
+
+    }
+}
