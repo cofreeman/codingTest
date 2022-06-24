0c5ac37714ec9ce704ef2952de1011ceadb20290
choijungwoo493 <jwoo1016@gmail.com>
2022-06-24 22:11:12 +0900

    버블정렬

--- src/sort/BubbleSort.java
+++ src/sort/BubbleSort.java
@@ -2,13 +2,8 @@
 package sort;
 
 public class BubbleSort {
-    /**
-     * 버블 정렬 메서드를 구현하세요.
-     *
-     * 구현이 끝나면
-     * 자신이 리드미컬하게 구현했다면 -->  bubbleSort5 와 같은 결과가 나와야합니다.
-     */
-    static int[] array = new int[]{1,2,5,6,7,3,4,2};
+
+    static int[] array = new int[]{1, 2, 5, 6, 7, 3, 4, 2};
 
     public static void main(String[] args) {
 
@@ -16,6 +11,20 @@ public class BubbleSort {
     }
 
     private static void bubbleSort(int[] array) {
-
+        int size = array.length;
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size - i - 1; j++) {
+                if (array[j] > array[j + 1]) {
+                    swap(j, j + 1);
+                    /**
+                     * 리드미컬한 구현을 했으면 여기서 bubbleSort 메서드 구현은 끝입니다.
+                     * 리드미컬한 구현은
+                     * array[j] 값이 array[j + 1] 보다 크면 바꿔야지~ 라는 생각을하고 swap(j, j+1) 을 하고 끝납니다.
+                     * swap 의 구현은 나중일 입니다.
+                     * Go To 5-2
+                     */
+                }
+            }
+        }
     }
 }
