73851d4733941680c3d48706499b2a4370d5d350
choijungwoo493 <jwoo1016@gmail.com>
2022-06-24 21:32:12 +0900

    버블정렬

--- src/sort/BubbleSort.java
+++ src/sort/BubbleSort.java
@@ -4,12 +4,11 @@ package sort;
 public class BubbleSort {
     int[] array = new int[]{1,2,5,6,7,3,4,2};
 
-    //todo 일단 자바에서는 psvm 함수가 아니면 실행이 불가능하므로 main문을 만들어줍니다.
+    /**
+     *일단 자바에서는 psvm 함수가 아니면 실행이 불가능하므로 main 문을 만들어줍니다.
+     */
     public static void main(String[] args) {
 
     }
 
-    public static void bubbleSort(int array) {
-
-    }
 }
