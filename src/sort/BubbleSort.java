3c85b0e1cdf781e029a7a0d752d316d44c15cfaf
choijungwoo493 <jwoo1016@gmail.com>
2022-06-24 21:01:29 +0900

    버블정렬

--- src/sort/BubbleSort.java
+++ src/sort/BubbleSort.java
@@ -4,7 +4,7 @@ package sort;
 public class BubbleSort {
     int[] array = new int[]{1,2,5,6,7,3,4,2};
 
-    //todo: 버블 정렬을 이용해 주어진 배열을 오름차순으로 출력하세요
+    //todo: 버블 정렬을 이용해 주어진 배열을 오름차순으로 출력하세요(리드미컬하게)
     public static void bubbleSort(int array) {
 
     }
