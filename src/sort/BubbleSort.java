79ff02113b6a7ecfaa4bb153b61b2735564b1434
choijungwoo493 <jwoo1016@gmail.com>
2022-06-24 21:51:42 +0900

    버블정렬

--- src/sort/BubbleSort.java
+++ src/sort/BubbleSort.java
@@ -3,11 +3,9 @@ package sort;
 
 public class BubbleSort {
     /**
-     1. main method 가 array 라는 변수를 읽으려면 array 변수를 static 변수로 바꿔 클래스내의 모든 범위에서 사용할 수 있게하거나
-     2. main method 안에 new BubbleSort() 로 객체를 생성해 객체를 통해 array 변수를 참조하는 방법이 있습니다.
-     3. 또, main method 안에 array 변수를 넣어 변수의 scope 를 줄여 참조하는 방법이 있습니다.
+     * 이제 option + enter 로 bubbleSort 메서드를 선언합니다.
      */
-    int[] array = new int[]{1,2,5,6,7,3,4,2};
+    static int[] array = new int[]{1,2,5,6,7,3,4,2};
 
     public static void main(String[] args) {
 
