7b0b99c002299ee4f640f8b9eaddf9c74a9ce4ee
choijungwoo493 <jwoo1016@gmail.com>
2022-07-03 09:09:34 +0900

    스트림 문제 답

--- src/sort/BubbleSort.java
+++ src/sort/BubbleSort.java
@@ -1,10 +1,14 @@
 //question 문제가 잘못된 점이 있을까요?
 package sort;
 
+import java.util.Arrays;
+import java.util.Comparator;
+
 public class BubbleSort {
 
     public static void main(String[] args) {
         //todo: 다음 배열 요소의 중복을 제거하고, 다섯 글자 이상의 단어만, 내림차순으로 정렬하여 출력하라.
         String[] array = { "carrot", "apple", "banana", "egg", "meat", "apple" };
+        Arrays.stream(array).distinct().filter(s->s.length()>=5).sorted(Comparator.reverseOrder()).forEach(System.out::println);
     }
 }
